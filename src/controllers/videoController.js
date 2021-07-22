c72927f826e98aaf220877802d6ddd29bbd3040a
jooonyong <leejy0718@naver.com>
2021-07-22 19:41:02 +0900

    hi

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,27 +1,42 @@
 import Video from "../models/Video";
 
 export const home = async(req,res) =>{
-    try{
-        const videos = await Video.find({});
-        console.log(videos);
-        return res.render("home",{pageTitle: "Home", videos});
+    const videos = await Video.find({}).sort({createdAt:"desc"});
+    return res.render("home",{pageTitle: "Home", videos});
+
+}
+export const watch = async(req,res) => {
+    const {id} = req.params;
+    const video= await Video.findById(id);
+    if(video === null)
+    {
+        return res.render("404",{pageTitle: "Video not found."});
     }
-    catch{
-        res.render("server-error");
+    return res.render("watch", {pageTitle:video.title, video});
+}
+export const getEdit = async (req,res) => {
+    const {id} = req.params;
+    const video = await Video.findById(id);
+    if(!video)
+    {
+        return res.render("404",{pageTitle:"Video not found."});
     }
+    return res.render("edit",{pageTitle:`Edit ${video.title}`, video});
 }
-export const watch = (req,res) => {
-    const id = req.params.id;
-    return res.render("watch", {pageTitle:`Watching`});
-}
-export const getEdit = (req,res) => {
-    const id = req.params.id;
-    return res.render("edit",{pageTitle:`Editing:`});
-}
-export const postEdit = (req,res) =>{
-    const id = req.params.id;
-    const title = req.body.title;
-    console.log(req.body);
+export const postEdit = async(req,res) =>{
+    const {id} = req.params;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.exists({_id:id});
+    if(!video)
+    {
+        return res.render("404",{pageTitle:"Video not found."});
+    }
+    await Video.findByIdAndUpdate(id,{
+        title,
+        description,
+        hashtags:Video.formatHashtags(hashtags),
+    })
+    
     return res.redirect(`/video/${id}`);
 }
 
@@ -36,7 +51,7 @@ export const postUpload = async (req,res)=>{
             title,
             description,
             createdAt:Date.now(),
-            hashtags:hashtags.split(",").map(word=>`#${word}`),
+            hashtags:Video.formatHashtags(hashtags),
             meta:{
                 views:0,
                 rating:0,
@@ -46,6 +61,17 @@ export const postUpload = async (req,res)=>{
         return res.redirect("/");
     }
     catch(error){
-        return res.render("upload",{pageTitle:`Upload video`,errorMessage:error._message,});
+        return res.render("upload",{pageTitle:`Upload video`, errorMessage:error._message,});
     }
+}
+
+export const deleteVideo =async(req,res)=>{
+    const {id}= req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
+}
+
+export const search =(req,res)=>{
+    const {keyword}= req.query;
+    return res.render("search",{pageTitle:`Search`});
 }
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
-import {watch, getEdit,postEdit,getUpload,postUpload} from "../controllers/videoController";
+import {watch, getEdit,postEdit,getUpload,postUpload,deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)",watch);
-videoRouter.get("/:id(\\d+)/edit",getEdit);
-videoRouter.post("/:id(\\d+)/edit",postEdit);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})",watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.get("/upload",getUpload);
 videoRouter.post("/upload",postUpload);
 
